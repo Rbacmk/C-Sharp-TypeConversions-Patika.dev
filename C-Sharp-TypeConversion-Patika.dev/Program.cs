85b194eb14a63b1e4ae75b30b94319d5ae7a568b
bacmk <ozancakmak867@gmail.com>
2022-08-26 13:45:14 +0300

    Proje dosyası ekle.

---
+++ C-Sharp-TypeConversion-Patika.dev/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C_Sharp_TypeConversion_Patika.dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+# T-r-D-n-mleri
+            //Tip Dönüşümleri
+            //Biliçsiz Dönüşüm
+
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+            int d = a + b + c;
+            Console.WriteLine("d :"d);
+            long h = d;
+            Console.WriteLine("h:" + h);
+            float i = h;
+            Console.WriteLine("i:" + i);
+            string e = "zikriye";
+            char f = 'k';
+            object g = e + f + d;
+            Console.WriteLine("g:" + g);
+
+            // Bilinçli Tür Dönüşümleri
+            Console.WriteLine("Bilinçli tür dönüşümleri");
+            int x = 4;
+            byte y = (byte)x;
+            Console.WriteLine("y:" + y);
+
+        }
+    }
+}
